7da0c53ec1ef8b81e5446c03259872351f82ee0e
abomdevp <abomdevp@gmail.com>
2026-03-27 16:37:15 -0300

    fix/import-correcto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,11 +11,11 @@
 </template>
 
 <script setup>
-import Navbar from '../components/Navbar.vue'
+import Navbar from '../components/NavBar.vue'
 import HeroSection from '../components/HeroSection.vue'
 import AboutSection from '../components/AboutSection.vue'
 import ProjectsSection from '../components/ProjectsSection.vue'
 import SkillsSection from '../components/SkillsSection.vue'
 import ContactSection from '../components/ContactSection.vue'
 import FooterSection from '../components/FooterSection.vue'
-</script>
+</script>D
